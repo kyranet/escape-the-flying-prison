8c07d7224bb7c664f72662ba634a53ef86277625
Antonio Román <kyradiscord@gmail.com>
2020-08-27 11:37:43 +0200

    feat: add spawner in laser

---
+++ Assets/Scripts/Player/AgentCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class AgentCreator : MonoBehaviour
+    {
+        public float Distance = 8f;
+        [SerializeField] private Transform Weapon;
+        [SerializeField] private Transform Agents;
+        [SerializeField] private GameObject AgentPrefab;
+
+        public void Shoot()
+        {
+            var point = GetSpawnPoint();
+            Instantiate(AgentPrefab, point, Quaternion.identity, Agents);
+        }
+
+        private Vector3 GetSpawnPoint()
+        {
+            if (Physics.Raycast(transform.position, Weapon.transform.up, out var hit, Distance))
+            {
+                return hit.point;
+            }
+
+            return transform.position + Weapon.transform.up * Distance;
+        }
+    }
+}
--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -1,20 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player
 {
+    [RequireComponent(typeof(AgentCreator))]
     public class Shoot : MonoBehaviour
     {
         public GameObject Laser;
         public MonoBehaviour Controller;
+        private AgentCreator _ac;
+
+        private void Awake()
+        {
+            _ac = GetComponent<AgentCreator>();
+        }
 
         private void Update()
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 Laser.SetActive(true);
                 Controller.enabled = false;
+                _ac.Shoot();
             }
-            else if (Laser.activeSelf)
+            else if (Input.GetMouseButtonUp(0))
             {
                 Laser.SetActive(false);
                 Controller.enabled = true;
