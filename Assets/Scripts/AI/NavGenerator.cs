6c759eb873f6c9ba77a357aaa4cf4672b4706b6f
Antonio Román <kyradiscord@gmail.com>
2020-08-31 00:02:07 +0200

    feat: path routing done, untested

---
+++ Assets/Scripts/AI/NavGenerator.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+using Joint = AI.NavPlatform.NavPlatformJoint;
+
+namespace AI
+{
+    public class NavGenerator : MonoBehaviour
+    {
+        [CanBeNull] public NavPlatform Entry;
+        public List<NavPlatform> Platforms;
+
+        private void Awake()
+        {
+            Scan();
+            // GetRoute(new Vector3(17, 2, 0), new Vector3(30, -7, -28));
+        }
+
+        public void Scan()
+        {
+            if (Entry is null) return;
+
+            Platforms.Clear();
+            ScanPlatform(Entry);
+        }
+
+        [CanBeNull]
+        public NavPlatform GetPlatform(Vector3 position)
+        {
+            return Platforms.FirstOrDefault(platform => platform.Contains(position));
+        }
+
+        public Stack<Vector3> GetRoute(Vector3 a, Vector3 b)
+        {
+            if ((a - b).sqrMagnitude < 0.1f) return new Stack<Vector3>();
+
+            var pa = GetPlatform(a);
+            if (pa is null) return new Stack<Vector3>();
+
+            var pb = GetPlatform(b);
+            if (pb is null) return new Stack<Vector3>();
+
+            var stack = new Stack<Vector3>();
+            stack.Push(b);
+            if (pa == pb || GetPossibleRoute(pa, new List<NavPlatform>(), pb, ref stack, out _)) return stack;
+
+            // Return empty stack:
+            return new Stack<Vector3>();
+        }
+
+        private bool GetPossibleRoute(NavPlatform platform, ICollection<NavPlatform> scanned, NavPlatform target,
+            ref Stack<Vector3> stack, out float distance)
+        {
+            // Add the entry to the scan to avoid cyclic scans:
+            scanned.Add(platform);
+
+            distance = float.MaxValue;
+            Joint? shortestPlatform = null;
+
+            foreach (var joint in platform.Connections.Where(joint => !scanned.Contains(joint.Platform)))
+            {
+                // Check whether the platform we are scanning is the target:
+                if (joint.Platform == target)
+                {
+                    shortestPlatform = joint;
+                    break;
+                }
+
+                // If no route was found, skip:
+                if (!GetPossibleRoute(joint.Platform, scanned, target, ref stack, out var possibleDistance))
+                    continue;
+
+                // If the route was longer than the previous, skip:
+                if (possibleDistance >= distance) continue;
+
+                // Set the new shortest route:
+                distance = possibleDistance;
+                shortestPlatform = joint;
+            }
+
+            if (!shortestPlatform.HasValue) return false;
+
+            if (stack.Count != 1) stack.Push(shortestPlatform.Value.OutPosition);
+            stack.Push(shortestPlatform.Value.InPosition);
+            distance = shortestPlatform.Value.Distance;
+            return true;
+        }
+
+        private void ScanPlatform([CanBeNull] NavPlatform platform)
+        {
+            if (platform is null) return;
+
+            Platforms.Add(platform);
+            platform.Refresh();
+
+            foreach (var joint in platform.Connections.Where(joint => !Platforms.Contains(joint.Platform)))
+            {
+                ScanPlatform(joint.Platform);
+            }
+        }
+    }
+}
--- Assets/Scripts/AI/NavPlatform.cs
+++ Assets/Scripts/AI/NavPlatform.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AI
@@ -6,16 +7,71 @@ namespace AI
     public class NavPlatform : MonoBehaviour
     {
         public List<NavJoint> Joints;
-        public Vector3 Position;
-        public Vector3 Size;
+        public Bounds Bounds;
+
+        [Serializable]
+        public struct NavPlatformJoint
+        {
+            public NavPlatform Platform;
+
+            /// <summary>
+            /// Entrance's position.
+            /// </summary>
+            public Vector3 InPosition;
+
+            /// <summary>
+            /// Exit's position.
+            /// </summary>
+            public Vector3 OutPosition;
+
+            /// <summary>
+            /// The calculated distance from {InPosition} and {OutPosition}
+            /// </summary>
+            public float Distance => (InPosition - OutPosition).magnitude;
+        }
+        public List<NavPlatformJoint> Connections;
+
+        public Vector3 Position => transform.position;
+        public Vector3 Center => Position + Bounds.center;
+
+        public bool Contains(Vector3 position)
+        {
+            return Bounds.Contains(position - Position);
+        }
+
+        public void Refresh()
+        {
+            Connections.Clear();
+            foreach (var joint in Joints)
+            {
+                if (joint.A.Platform == this)
+                {
+                    Connections.Add(new NavPlatformJoint
+                    {
+                        Platform = joint.B.Platform,
+                        InPosition = joint.A.Position + joint.A.Platform.Position,
+                        OutPosition = joint.B.Position + joint.B.Platform.Position
+                    });
+                }
+                else
+                {
+                    Connections.Add(new NavPlatformJoint
+                    {
+                        Platform = joint.A.Platform,
+                        InPosition = joint.B.Position + joint.B.Platform.Position,
+                        OutPosition = joint.A.Position + joint.A.Platform.Position
+                    });
+                }
+            }
+        }
 
         private void OnDrawGizmos()
         {
-            var center = transform.position;
-            var boxCenter = center + Position;
+            var position = Position;
+            var center = Center;
 
             Gizmos.color = Color.green;
-            Gizmos.DrawWireCube(boxCenter, Size);
+            Gizmos.DrawWireCube(center, Bounds.size);
 
             // Draw joints
             Gizmos.color = Color.blue;
@@ -23,11 +79,11 @@ namespace AI
             {
                 if (joint.A.Platform == this)
                 {
-                    Gizmos.DrawLine(boxCenter, center + joint.A.Position);
+                    Gizmos.DrawLine(center, position + joint.A.Position);
                 }
                 else if (joint.B.Platform == this)
                 {
-                    Gizmos.DrawLine(boxCenter, center + joint.B.Position);
+                    Gizmos.DrawLine(center, position + joint.B.Position);
                 }
             }
         }
