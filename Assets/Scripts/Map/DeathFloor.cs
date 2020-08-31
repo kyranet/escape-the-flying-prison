b37c7b825b8aae7599276881ed549d5b549c56b6
Antonio Román <kyradiscord@gmail.com>
2020-08-31 13:15:53 +0200

    feat: added some decoration

--- Assets/Scripts/Map/DeathFloor.cs
+++ Assets/Scripts/Map/DeathFloor.cs
@@ -7,11 +7,15 @@ namespace Map
     [RequireComponent(typeof(MeshCollider))]
     public class DeathFloor : MonoBehaviour
     {
-        private void OnCollisionEnter(Collision other)
+        private void OnTriggerEnter(Collider collider)
         {
-            if (other.transform.CompareTag("Player"))
+            if (collider.transform.CompareTag("Player"))
             {
                 SceneManager.LoadSceneAsync("Scenes/Game");
+            }
+            else if (collider.transform.CompareTag("NPC"))
+            {
+                Destroy(collider.transform.gameObject);
             }
         }
     }
